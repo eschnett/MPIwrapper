5bc7f5155dcaaf4214968690e80b8d3be1db3caa
Erik Schnetter <schnetter@gmail.com>
2022-02-27 16:25:38 -0500

    Allow STATUS_SIZE <=

--- mpiwrapper.f
+++ mpiwrapper.f
@@ -2,9 +2,9 @@
       implicit none
       include "mpif.h"
 
-!     Assert that MPI_STATUS_SIZE = 6
+!     Assert that MPI_STATUS_SIZE <= 6
       integer cond
-      parameter (cond = MPI_STATUS_SIZE - 6)
+      parameter (cond = min(0, MPI_STATUS_SIZE - 6))
       integer(1 - abs(cond)) check
 
       include "mpiwrapper_definitions_fortran.h"
